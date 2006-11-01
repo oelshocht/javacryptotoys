1e6a7d0ff17de1685659a97e54f2453c2f946b6d
Olivier Elshocht <olivier.elshocht@gmail.com>
2006-11-01 21:22:36 +0000

    Initial commit.

---
+++ src/security/Dsa.java
@@ -0,0 +1,222 @@
+/* Copyright (c) 2006 Olivier Elshocht
+ *
+ * Dsa.java
+ *
+ * Created on 1 novembre 2006, 17:27
+ */
+
+package security;
+
+import java.io.*;
+import java.security.*;
+import java.security.spec.*;
+
+/**
+ *
+ * @author Olivier Elshocht
+ */
+public class Dsa {
+
+    /** Creates a new instance of Dsa */
+    public Dsa() {
+    }
+
+    public static void generateKeyPair(String keyId) {
+
+        System.out.println("Generating 1024-bit DSA key pair " + keyId + "...");
+
+        try {
+            // Generate key pair.
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DSA", "SUN");
+            SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
+            keyGen.initialize(1024, random);
+
+            KeyPair keyPair = keyGen.generateKeyPair();
+            PrivateKey privKey = keyPair.getPrivate();
+            PublicKey pubKey = keyPair.getPublic();
+            byte[] encodedPrivKey = privKey.getEncoded();
+            byte[] encodedPubKey = pubKey.getEncoded();
+
+            // Save encoded keys to file.
+            FileOutputStream privFos = new FileOutputStream(keyId + ".privkey");
+            privFos.write(encodedPrivKey);
+            privFos.close();
+            FileOutputStream pubFos = new FileOutputStream(keyId + ".pubkey");
+            pubFos.write(encodedPubKey);
+            pubFos.close();
+
+            // Save encoded keys to Java source code.
+            PrintWriter privWriter = new PrintWriter(keyId + ".privkey.java");
+            privWriter.print("byte[] " + keyId + "PrivKey = { " + encodedPrivKey[0]);
+            for (int i = 1; i < encodedPrivKey.length; ++i) {
+                privWriter.print(", " + encodedPrivKey[i]);
+            }
+            privWriter.println(" };");
+            privWriter.close();
+
+            PrintWriter pubWriter = new PrintWriter(keyId + ".pubkey.java");
+            pubWriter.print("byte[] " + keyId + "PubKey = { " + encodedPubKey[0]);
+            for (int i = 1; i < encodedPubKey.length; ++i) {
+                pubWriter.print(", " + encodedPubKey[i]);
+            }
+            pubWriter.println(" };");
+            pubWriter.close();
+
+            // Print some info.
+            System.out.println("Private key algorithm: " + privKey.getAlgorithm());
+            System.out.println("Private key format: " + privKey.getFormat());
+            System.out.println("Public key algorithm: " + pubKey.getAlgorithm());
+            System.out.println("Public key format: " + pubKey.getFormat());
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void sign(String keyId, String filename) {
+
+        System.out.println("Signing file " + filename + " with key " + keyId + "...");
+
+        try {
+            // Read encoded private key.
+            FileInputStream privFis = new FileInputStream(keyId + ".privkey");
+            byte[] encodedPrivKey = new byte[privFis.available()];
+            privFis.read(encodedPrivKey);
+            privFis.close();
+
+            // Decode private key.
+            PKCS8EncodedKeySpec privKeySpec = new PKCS8EncodedKeySpec(encodedPrivKey);
+            KeyFactory keyFactory = KeyFactory.getInstance("DSA", "SUN");
+            PrivateKey privKey = keyFactory.generatePrivate(privKeySpec);
+
+            // Sign the file.
+            Signature signature = Signature.getInstance("SHA1withDSA", "SUN");
+            signature.initSign(privKey);
+
+            BufferedInputStream in = new BufferedInputStream(new FileInputStream(filename));
+            byte[] buffer = new byte[4000];
+            int size;
+            int fileSize = 0;
+
+            while (0 < (size = in.read(buffer))) {
+                signature.update(buffer, 0, size);
+                fileSize += size;
+            }
+            in.close();
+            byte[] encodedSignature = signature.sign();
+            int signatureSize = encodedSignature.length;
+
+            // Store file and signature.
+            BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(filename + "." + keyId + ".sign"));
+            in = new BufferedInputStream(new FileInputStream(filename));
+
+            out.write(fileSize & 0xFF);
+            out.write((fileSize >> 8) & 0xFF);
+            out.write((fileSize >> 16) & 0xFF);
+            out.write((fileSize >> 24) & 0xFF);
+            out.write(signatureSize & 0xFF);
+            out.write((signatureSize >> 8) & 0xFF);
+            out.write((signatureSize >> 16) & 0xFF);
+            out.write((signatureSize >> 24) & 0xFF);
+
+            while (0 < (size = in.read(buffer))) {
+                out.write(buffer, 0, size);
+            }
+            out.write(encodedSignature);
+            out.close();
+            in.close();
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static byte[] verify(String keyId, String filename) {
+
+        byte[] data = null;
+
+        try {
+            // Read encoded public key.
+            System.out.println("Reading public key " + keyId + "...");
+            FileInputStream pubFis = new FileInputStream(keyId + ".pubkey");
+            byte[] encodedPubKey = new byte[pubFis.available()];
+            pubFis.read(encodedPubKey);
+            pubFis.close();
+
+            data = verify(encodedPubKey, filename);
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+
+        return data;
+    }
+
+    public static byte[] verify(byte[] encodedPubKey, String filename) {
+
+        byte[] data = null;
+
+        try {
+            // Decode public key.
+            System.out.println("Decoding public key...");
+            X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(encodedPubKey);
+            KeyFactory keyFactory = KeyFactory.getInstance("DSA", "SUN");
+            PublicKey pubKey = keyFactory.generatePublic(pubKeySpec);
+
+            data = verify(pubKey, filename);
+        }
+        catch (Exception e) {
+            System.out.println(e);            
+        }
+
+        return data;
+    }
+
+    public static byte[] verify(PublicKey pubKey, String filename) {
+
+        System.out.println("Verifying signature of file " + filename + "...");
+        byte[] data = null;
+
+        try {
+            // Read file and signature.
+            BufferedInputStream in = new BufferedInputStream(new FileInputStream(filename));
+            int fileSize = 0;
+            int signatureSize = 0;
+
+            fileSize = in.read();
+            fileSize += in.read() << 8;
+            fileSize += in.read() << 16;
+            fileSize += in.read() << 24;
+            signatureSize = in.read();
+            signatureSize += in.read() << 8;
+            signatureSize += in.read() << 16;
+            signatureSize += in.read() << 24;
+            System.out.println("File size: " + fileSize);
+            System.out.println("Signature size: " + signatureSize);
+
+            byte[] file = new byte[fileSize];
+            byte[] encodedSignature = new byte[signatureSize];
+            in.read(file, 0, fileSize);
+            in.read(encodedSignature, 0, signatureSize);
+            in.close();
+
+            // Verify the signature.
+            Signature signature = Signature.getInstance("SHA1withDSA", "SUN");
+            signature.initVerify(pubKey);
+            signature.update(file, 0, fileSize);
+
+            if (signature.verify(encodedSignature)) {
+                System.out.println("Signature verification successful.");
+                data = file;
+            }
+            else {
+                System.out.println("Signature verification failed.");
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+
+        return data;        
+    }
+}
---
+++ src/security/Main.java
@@ -0,0 +1,62 @@
+/* Copyright (c)2006 Olivier Elshocht
+ *
+ * Main.java
+ *
+ * Created on 1 novembre 2006, 17:09
+ */
+
+package security;
+
+/**
+ *
+ * @author Olivier Elshocht
+ */
+public class Main {
+
+    /** Creates a new instance of Main */
+    public Main() {
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Parse arguments and options.
+        boolean isValidCommand = false;
+
+        if (args.length == 2) {
+            if (args[0].equals("test")) {
+                byte[] securityPubKey = { 48, -126, 1, -72, 48, -126, 1, 44, 6, 7, 42, -122, 72, -50, 56, 4, 1, 48, -126, 1, 31, 2, -127, -127, 0, -3, 127, 83, -127, 29, 117, 18, 41, 82, -33, 74, -100, 46, -20, -28, -25, -10, 17, -73, 82, 60, -17, 68, 0, -61, 30, 63, -128, -74, 81, 38, 105, 69, 93, 64, 34, 81, -5, 89, 61, -115, 88, -6, -65, -59, -11, -70, 48, -10, -53, -101, 85, 108, -41, -127, 59, -128, 29, 52, 111, -14, 102, 96, -73, 107, -103, 80, -91, -92, -97, -97, -24, 4, 123, 16, 34, -62, 79, -69, -87, -41, -2, -73, -58, 27, -8, 59, 87, -25, -58, -88, -90, 21, 15, 4, -5, -125, -10, -45, -59, 30, -61, 2, 53, 84, 19, 90, 22, -111, 50, -10, 117, -13, -82, 43, 97, -41, 42, -17, -14, 34, 3, 25, -99, -47, 72, 1, -57, 2, 21, 0, -105, 96, 80, -113, 21, 35, 11, -52, -78, -110, -71, -126, -94, -21, -124, 11, -16, 88, 28, -11, 2, -127, -127, 0, -9, -31, -96, -123, -42, -101, 61, -34, -53, -68, -85, 92, 54, -72, 87, -71, 121, -108, -81, -69, -6, 58, -22, -126, -7, 87, 76, 11, 61, 7, -126, 103, 81, 89, 87, -114, -70, -44, 89, 79, -26, 113, 7, 16, -127, -128, -76, 73, 22, 113, 35, -24, 76, 40, 22, 19, -73, -49, 9, 50, -116, -56, -90, -31, 60, 22, 122, -117, 84, 124, -115, 40, -32, -93, -82, 30, 43, -77, -90, 117, -111, 110, -93, 127, 11, -6, 33, 53, 98, -15, -5, 98, 122, 1, 36, 59, -52, -92, -15, -66, -88, 81, -112, -119, -88, -125, -33, -31, 90, -27, -97, 6, -110, -117, 102, 94, -128, 123, 85, 37, 100, 1, 76, 59, -2, -49, 73, 42, 3, -127, -123, 0, 2, -127, -127, 0, -97, -1, 8, 40, -41, 40, 24, 73, 16, -91, 24, 55, -33, -128, 79, 87, 29, 22, -94, 24, 106, 53, -96, -119, 55, 5, 120, -59, -42, 11, -57, -123, 17, 87, 76, -50, -76, -60, 99, 26, 79, 34, 75, 101, 118, 121, -94, 86, -34, 79, 93, 113, 102, -7, -108, 99, -92, -51, 74, 25, 55, -69, -100, -66, -29, -8, 55, -85, 62, -20, -48, -56, 98, -29, 4, -31, 53, 16, 78, 60, 105, -14, -4, 91, 47, 45, 126, 106, 11, 76, 122, 86, -81, -128, 75, -85, 86, -113, 72, 104, 43, -25, 49, 31, 24, 88, -112, 41, -109, 27, 24, 29, 126, 110, -119, -11, -43, 45, -106, -47, 87, 12, -33, -101, 70, 49, 123, 48 };
+                Dsa.verify(securityPubKey, args[1]);
+                isValidCommand = true;
+            }
+            else if (args[0].equals("genkey")) {
+                Dsa.generateKeyPair(args[1]);
+                isValidCommand = true;
+            }
+        }
+        else if (args.length == 3) {
+            if (args[0].equals("sign")) {
+                Dsa.sign(args[1], args[2]);
+                isValidCommand = true;
+            }
+            else if (args[0].equals("verify")) {
+                byte[] data = Dsa.verify(args[1], args[2]);
+                System.out.println("Data: " + data);
+                isValidCommand = true;
+            }
+        }
+
+        if (!isValidCommand) {
+            // If arguments are not valid, print usage.
+            System.out.println("Usage: security COMMAND [OPTION...]");
+            System.out.println();
+            System.out.println("Commands:");
+            System.out.println("    genkey KEYID         Generate a DSA key pair.");
+            System.out.println("    sign   KEYID FILE    Sign file with specified private key.");
+            System.out.println("    verify KEYID FILE    Verify file signature with specified public key.");
+            System.out.println("    test   FILE          Verify file signature with default public key.");
+        }
+    }
+}
